1cd18c757bf529ecbdf4cac9763e653f04eb7c1f
Eskimo <52078955+EskimoCold@users.noreply.github.com>
2026-08-12 20:07:52 +0300

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = [0] * len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures) - 1, -1, -1):
+            current_t = temperatures[i]
+            
+            while stack and temperatures[stack[-1]] <= temperatures[i]:
+                stack.pop()
+
+            if stack:
+                result[i] = stack[-1] - i
+
+            stack.append(i)
+
+        return result
